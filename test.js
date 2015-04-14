a596b0bed13908bfb774e93275b556f9b843762f
Nemo <viole_day@live.com>
2015-04-14 22:54:47 +0800

    fix test

--- test.js
+++ test.js
@@ -2,10 +2,9 @@
  * Created by Nemo on 15/4/14.
  */
 var Client = require('./index');
-var client = new Client('http://172.16.10.25:8888', 'sessionService');
-client.rpcInvoke('checkToken', {access_token: 'cb59967b-cad0-4359-bd2a-8e3221aee331'}).then(function (result) {
+var sessionService = new Client('http://172.16.10.25:8888', 'sessionService');
+sessionService.rpcInvoke('checkToken', {access_token: 'cb59967b-cad0-4359-bd2a-8e3221aee331'}).then(function (result) {
   console.log(arguments);
 }, function (err) {
-  console.log(arguments);
   console.error(err);
 });
