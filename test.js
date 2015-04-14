6d518342b64562ce6ba26a4ce6824e7bc4885d1e
Nemo <viole_day@live.com>
2015-04-14 15:03:28 +0800

    完善err.rowData

--- test.js
+++ test.js
@@ -3,8 +3,9 @@
  */
 var Client = require('./index');
 var client = new Client('http://172.16.10.25:8888', 'sessionService');
-client.rpcInvoke('checkToken', {access_token: '111'}).then(function (result) {
+client.rpcInvoke('checkToken', {access_token: 'cb59967b-cad0-4359-bd2a-8e3221aee331'}).then(function (result) {
   console.log(arguments);
 }, function (err) {
+  console.log(arguments);
   console.error(err);
 });
