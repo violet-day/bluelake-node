f8a665f09627cd20db3daf8242b04ad46c500247
Nemo <viole_day@live.com>
2015-06-09 13:11:25 +0800

    argMap的value需要JSON.stringify fix it

--- test.js
+++ test.js
@@ -3,8 +3,8 @@
  */
 var Client = require('./index');
 var sessionService = new Client('http://172.16.10.25:8888', 'sessionService');
-sessionService.rpcInvoke('checkToken', {access_token: 'cb59967b-cad0-4359-bd2a-8e3221aee331'}).then(function (result) {
-  //console.log(result);
+sessionService.rpcInvoke('checkToken', {access_token: JSON.stringify('ad415cff-1a3a-43c5-90a4-ff96a0b05b41')}).then(function (result) {
+  console.log(result);
 }, function (err) {
-  //console.error(err);
+  console.error(err);
 });
