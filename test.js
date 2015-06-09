af99f3d2aa169ca427f9e4d5987e3c5ae84140ee
Nemo <viole_day@live.com>
2015-06-09 18:10:17 +0800

    add json.stringify in argMap

--- test.js
+++ test.js
@@ -3,7 +3,7 @@
  */
 var Client = require('./index');
 var sessionService = new Client('http://172.16.10.25:8888', 'sessionService');
-sessionService.rpcInvoke('checkToken', {access_token: JSON.stringify('ad415cff-1a3a-43c5-90a4-ff96a0b05b41')}).then(function (result) {
+sessionService.rpcInvoke('checkToken', {access_token: 'ad415cff-1a3a-43c5-90a4-ff96a0b05b42'}).then(function (result) {
   console.log(result);
 }, function (err) {
   console.error(err);
