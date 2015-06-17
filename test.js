eabe0c5017b1d4c98ee0035c0244760a4dbfa111
Nemo <viole_day@live.com>
2015-06-17 11:02:31 +0800

    add pretty-hrtime package

--- test.js
+++ test.js
@@ -3,7 +3,7 @@
  */
 var Client = require('./index');
 var sessionService = new Client('http://172.16.10.25:8888', 'sessionService');
-sessionService.rpcInvoke('checkToken', {access_token: 'ad415cff-1a3a-43c5-90a4-ff96a0b05b42'}).then(function (result) {
+sessionService.rpcInvoke('checkToken', {access_token: '621a4ef5-8592-432e-af9e-727472706223'}).then(function (result) {
   console.log(result);
 }, function (err) {
   console.error(err);
